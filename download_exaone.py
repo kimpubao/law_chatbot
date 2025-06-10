07290e53d654a0599ca1983f58bb903a353f9845
root <root@DESKTOP-MEP78DF>
2025-06-10 17:53:40 +0900

    2.4B 모델 다운로드 코드 추가

--- download_exaone.py
+++ download_exaone.py
@@ -1,7 +1,15 @@
 from huggingface_hub import snapshot_download
 
+# 기존 7.8B 모델 다운로드 코드
+# snapshot_download(
+#     repo_id="LGAI-EXAONE/EXAONE-3.5-7.8B-instruct",
+#     local_dir="EXAONE-3.5-7.8B-Instruct",
+#     local_dir_use_symlinks=False
+# )
+
+# 새롭게 3.5B 모델 다운로드
 snapshot_download(
-    repo_id="LGAI-EXAONE/EXAONE-3.5-7.8B-instruct",
-    local_dir="EXAONE-3.5-7.8B-Instruct",
+    repo_id="LGAI-EXAONE/EXAONE-3.5-2.4B-instruct",
+    local_dir="EXAONE-3.5-2.4B-Instruct",
     local_dir_use_symlinks=False
 )
